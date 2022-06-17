3c15dffb7bb7083825604a406f887c21f5a12747
bluffgnuff <stefano.romeo@studio.unibo.it>
2022-06-17 13:49:49 +0200

    pole execercise work

--- Pole-ex/pole.py
+++ Pole-ex/pole.py
@@ -1,1 +1,14 @@
- 
+import gym
+env = gym.make("CartPole-v1")
+observation, info = env.reset(seed=42, return_info=True)
+
+for _ in range(100000):
+    ## Permit to show 
+    env.render()
+    action = env.action_space.sample()
+    observation, reward, done, info = env.step(action)
+    print( "observation=", observation, "info=", info, "reward=", reward, "\n")
+    if done:
+        observation, info = env.reset(return_info=True)
+
+env.close()
