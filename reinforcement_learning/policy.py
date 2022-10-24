b167c2f82cf5ee00c616987efd083deae0f9cb56
bluffgnuff <stefano.romeo@studio.unibo.it>
2022-10-24 17:51:42 +0200

    buffer updated

--- reinforcement_learning/policy.py
+++ reinforcement_learning/policy.py
@@ -8,7 +8,7 @@ class EpsilonGreedyPolicy:
         self.model = model
         self.action_space_size = action_space_size
         self.min_epsilon = min_epsilon
-        self.episode = 1tra
+        self.episode = 1
         self.episodes = episodes
         self.decay_rate = decay_rate
 
